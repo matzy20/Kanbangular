76e91efaf28dbae7473f593f6e28876148aa3ba6
Gail Matsushima <gail.matsushima@gmail.com>
2016-02-27 17:09:33 -1000

    all routes working via postman CRUD bud

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var db = require('./models');
 var express = require('express');
 var path = require('path');
 var sequelize = require ('sequelize');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -9,9 +11,22 @@ var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.resolve(__dirname, 'Public')));
 
-app.get('/api/', function (req, res){
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
+
+app.get('/api/cards', function (req, res){
   return db.Card.findAll({}).then(function(cards){
     res.json(cards);
+  });
+});
+
+app.get('/api/cards/:id', function (req, res) {
+  db.Card.find({
+    where: {
+      id: req.params.id
+    }
+  }).then(function (card){
+    res.json(card);
   });
 });
 
@@ -29,6 +44,36 @@ app.post('/api/cards', function (req, res){
     .catch(function (err){
       console.log(err);
     });
+});
+
+app.put('/api/cards/edit/:id', function (req, res){
+  db.Card.update({
+    person: req.body.person,
+    title: req.body.title,
+    descrip: req.body.descrip,
+    currentStatus: req.body.currentStatus,
+    completionDueDate: req.body.completionDueDate,
+    lastUpdated: req.body.lastUpdated
+  },
+  {
+    where: {
+      id: req.params.id
+    },
+    returning: true
+  }
+  ).then(function (card){
+    res.json(card);
+  });
+});
+
+app.delete('/api/cards/delete/:id', function (req, res){
+  db.Card.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function (card){
+    res.send("This card " + req.params.id + " has been deleted");
+  });
 });
 
 db.sequelize
