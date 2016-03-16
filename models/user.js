9321ccd3807e3336159d681a52c78f419c59d9ca
Gail Matsushima <gail.matsushima@gmail.com>
2016-03-15 19:49:26 -1000

    added hash settermethod to user Model now merging hashing branch to master

--- models/user.js
+++ models/user.js
@@ -1,10 +1,40 @@
+// 'use strict';
+// module.exports = function(sequelize, DataTypes) {
+//   var User = sequelize.define('User', {
+//     username: DataTypes.STRING,
+//     password: DataTypes.STRING
+//   }, {
+//     classMethods: {
+//       associate: function(models) {
+//         // associations can be defined here
+//       }
+//     }
+//   });
+//   return User;
+// };
+
+
 'use strict';
+var crypto = require('crypto');
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING,
+    username: {
+      validate: {
+        isAlphanumeric: true
+      },
+      type: DataTypes.STRING
+    },
     password: DataTypes.STRING
   }, {
+    setterMethods: {
+      password: function (password) {
+        var hashed = hashPassword(password);
+        this.setDataValue('password', hashed);
+      }
+    },
     classMethods: {
+      hashPassword: hashPassword,
       associate: function(models) {
         // associations can be defined here
       }
@@ -12,3 +42,7 @@ module.exports = function(sequelize, DataTypes) {
   });
   return User;
 };
+
+function hashPassword (password) {
+  return crypto.createHash('sha256').update(password).digest('hex');
+}
